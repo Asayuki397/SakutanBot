d79dd6c64032089b3facbea1b2cb3ededb3b418b
MorningSnow <97608776+Morningsno3@users.noreply.github.com>
2023-04-14 08:25:55 +0900

    arisa on discord

---
+++ run_discord.py
@@ -0,0 +1,23 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import os
+import asyncio
+import json
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+application_id = os.environ['APPLICATION_ID']
+prefix = [prefix for prefix in os.environ['BOT_PREFIX'].split('-')]
+intents=discord.Intents.all()
+
+bot = commands.Bot(command_prefix=prefix, intents=intents, application_id = application_id)
+bot.owner_id = os.environ['BOT_OWNER_ID']
+
+@bot.event
+async def on_ready():
+    print("ARiSA logged in.")
+    game = discord.Game("ARiSA 채팅방")
+    await bot.change_presence(status=discord.Status.online, activity=game)
+
+asyncio.run(load_cogs())
+bot.run(f"{BOT_TOKEN}")
